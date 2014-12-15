16b57d92549ebd0fec6b0a9076ebc365dd8024cf
Magnus Bjuvensjö <bjuvensjo@gmail.com>
2014-12-15 22:40:58 +0100

    added rovar-spraket.el

---
+++ my-defuns/rovar-spraket.el
@@ -0,0 +1,14 @@
+(defun mbj/rövarspråket()
+  (interactive)
+  (save-excursion
+    (goto-char 1)
+    (while (not (eobp))
+      (when (member (following-char) '(?b ?c ?d ?f ?g ?h ?j ?k ?l ?m ?n ?p ?q ?r ?s ?t ?v ?w ?x ?z ?B ?C ?D ?F ?G ?H ?J ?K ?L ?M ?N ?P ?Q ?R ?S ?T ?V ?W ?X ?Z))
+        (insert (string (following-char) ?o)))
+      (forward-char 1))))
+
+;;(global-set-key (kbd "C-c C-s") 'mbj/rövarspråket)
+
+
+
+
