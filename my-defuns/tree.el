31f64aa0f92bb129a27f3625df7e4b9104bf3bde
Magnus Bjuvensjö <bjuvensjo@gmail.com>
2015-02-22 16:09:38 +0100

    updated tree

--- my-defuns/tree.el
+++ my-defuns/tree.el
@@ -3,26 +3,18 @@
 Inserts a tree listing of the specified dir in current buffer)
 "
   (interactive "D")  
-  (defun inner-tree(dir depth)
-    (let ((buffer "")
-          (prefix "|")
-          (n depth)
-          (files (directory-files dir t ".*[a-zA-Z].*")))
-      (while (> n 0)
-        (setq prefix (concat prefix " |"))
-        (setq n (- n 1)))
+  (defun inner-tree(dir prefix)
+    (let ((absolute-files (directory-files dir t ".*[a-zA-Z].*"))
+          (files (directory-files dir nil ".*[a-zA-Z].*")))
       (while files
-        (setq buffer (concat buffer
-                             prefix
-                             "____"
-                             (replace-regexp-in-string ".*[\\]" "" ;; windows
-                                                       (replace-regexp-in-string ".*\/" "" (car files))) ;; posix
-                             "\n"
-                             (if (car (file-attributes (car files)))
-                                 (inner-tree (car files) (+ 1 depth))
-                               "")))
-        (setq files (cdr files)))
-      buffer))
-  (insert (inner-tree dir 1)))
+        (insert prefix)
+        (insert "____")
+        (insert (car files))
+        (insert "\n")
+        (when (car (file-attributes (car absolute-files)))
+          (inner-tree (car absolute-files) (concat prefix " |")))
+        (setq files (cdr files))
+        (setq absolute-files (cdr absolute-files)))))
+  (inner-tree dir "|"))
 
-(provide 'mbj/tree')
+(provide 'mbj/tree)
