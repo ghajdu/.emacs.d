a03bdde84db7203d52b1abbc5c33f67a212f9b86
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2016-11-24 17:17:48 +0100

    mc updates

--- my-defuns/mc.el
+++ my-defuns/mc.el
@@ -3,24 +3,18 @@
 (require 'url)
 
 ;; Modify below vars as suits your needs
+(defvar mbj/bitbucket-default-username "ei4577")
+(defvar mbj/bitbucket-project-keys-file "~/.bitbucket-project-keys")
 (defvar mbj/bitbucket-url-rest-api "http://cuso.edb.se/stash/rest/api/1.0/")
-(defvar mbj/bitbucket-project-keys '("LPLAN"
-                                     "LHAUTO"
-                                     "LPLANO"
-                                     "LPLANT"
-                                     "SBAB-LP"
-                                     "SBAB-LP-AUTOMATION"
-                                     "SBAB-LP-SERVICE"
-                                     "SBAB-LP-TMP"))
 
 (defun mbj/bitbucket-clone-repos (project repo-regexp out-dir username password)
   "Clone Bitbucket repos."
   (interactive
    (list
-    (completing-read "Project: " mbj/bitbucket-project-keys)
+    (completing-read "Project: " (mbj/bitbucket-get-project-keys))
     (read-string "Repo regexp (.*): " nil nil ".*")
     (read-directory-name "Output dir: ")
-    (read-string "Username (ei4577): " nil nil "ei4577")
+    (read-string (concat "Username (" mbj/bitbucket-default-username "): ") nil nil mbj/bitbucket-default-username)
     (read-passwd "Password: ")))
   (let ((url-request-method "GET")
         (url-request-extra-headers
@@ -44,21 +38,37 @@
                       (async-shell-command (buffer-string)))
                     (list out-dir repo-regexp)))))
 
-;; (let ((url-request-method "GET")
-;;       (url-request-extra-headers
-;;        (list (cons "Authorization" bitbucket-authorization) (cons "Content-Type" "application/json;charset=UTF-8"))))
-;;   (url-retrieve (concat bitbucket-url-rest-api "projects?limit=1000")
-;;                 (lambda (status)
-;;                   (switch-to-buffer (current-buffer))
-;;                   (keep-lines "{.*" (point-min) (point-max))
-;;                   (mark-whole-buffer)
-;;                   (kill-ring-save (point-min) (point-max))
-;;                   (create-scratch-buffer)
-;;                   (yank)
-;;                   (json-pretty-print-buffer)
-;;                   (keep-lines "key" (point-min) (point-max))
-;;                   (while (re-search-forward ".*key\": \\(.*\\)" nil t)
-;;                     (replace-match "\\1" nil nil))
-;;                   )))
+(defun mbj/bitbucket-get-project-keys ()
+  "Gets the Bitbucket projects keys that are 'cached' in mbj/bitbucket-project-keys-file"
+  (interactive)
+  (split-string (with-temp-buffer
+                  (insert-file-contents mbj/bitbucket-project-keys-file)
+                  (buffer-string)) "\n" t))
+
+(defun mbj/bitbucket-update-project-keys (username password)
+  "Updates the Bitbucket projects keys that are 'cached' in mbj/bitbucket-project-keys-file"
+  (interactive
+   (list
+    (read-string (concat "Username (" mbj/bitbucket-default-username "): ") nil nil mbj/bitbucket-default-username)
+    (read-passwd "Password: ")))
+  (let ((url-request-method "GET")
+        (url-request-extra-headers
+         (list (cons "Content-Type" "application/json")
+               (cons "Authorization" (concat "Basic " (base64-encode-string (concat username ":" password)))))))
+    (save-excursion
+      (url-retrieve (concat bitbucket-url-rest-api "projects?limit=1000")
+                    (lambda (status)
+                      (switch-to-buffer (current-buffer))
+                      (keep-lines "{.*" (point-min) (point-max))
+                      (mark-whole-buffer)
+                      (kill-ring-save (point-min) (point-max))
+                      (create-scratch-buffer)
+                      (yank)
+                      (json-pretty-print-buffer)
+                      (keep-lines "key" (point-min) (point-max))
+                      (while (re-search-forward ".*key\": \"\\(.*\\)\"" nil t)
+                        (replace-match "\\1" nil nil))
+                      (write-file mbj/bitbucket-project-keys-file)
+                      (kill-buffer))))))
 
 (provide 'mc)
