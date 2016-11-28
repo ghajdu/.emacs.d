c2fb379138ccd6bd0b8ef8a547a895368506f7c6
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2016-11-28 08:44:26 +0100

    tree

--- my-defuns/tree.el
+++ my-defuns/tree.el
@@ -1,15 +1,29 @@
-(defun mbj/tree (dir)
-  "
-Inserts a tree listing of the specified dir in current buffer)
+;; Modify below vars as suits your needs
+(defvar mbj/tree-ignored-names '(".git" "target" ".DS_Store" ".settings" ".idea" "*.iml" "*.iws"))
+
+(defun mbj/tree (all dir)
+  "Inserts a tree listing of the specified dir in current buffer. 
+If called without a prefix argument, the files specified in the variable mbj/tree-ignored-names are ignored.
+If called with a prefix argument, all files are listed.
 "
-  (interactive "D")  
-  (defun tree(dir prefix)
-    (let ((absolute-files (directory-files dir t ".*[a-zA-Z].*"))
-          (files (directory-files dir nil ".*[a-zA-Z].*")))
+  (interactive "P\nD")
+  (defun ignore-name (name)
+    (defun inner (ignored-names name)
+      (if (not ignored-names)
+          nil
+        (if (string-match-p (car ignored-names) name)
+            t
+          (inner (cdr ignored-names) name))))
+    (inner mbj/tree-ignored-names name))  
+  (defun tree (dir prefix)
+    (let ((absolute-files (directory-files dir t ".*[[:alnum:]].*"))
+          (files (directory-files dir nil ".*[[:alnum:]].*")))
       (while files
-        (insert (concat prefix (car files) "\n"))
-        (when (car (file-attributes (car absolute-files)))
-          (tree (car absolute-files) (concat "| " prefix)))
+        (when (or all (not (ignore-name (car files))))
+          (progn
+            (insert (concat prefix (car files) "\n"))
+            (when (car (file-attributes (car absolute-files)))
+              (tree (car absolute-files) (concat "| " prefix)))))
         (setq files (cdr files))
         (setq absolute-files (cdr absolute-files)))))
   (save-excursion
