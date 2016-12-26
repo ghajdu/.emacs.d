1417b8af364e14c60b0fc5e74540754cfd5cf32d
Magnus Bjuvensjö <bjuvensjo@gmail.com>
2016-12-26 20:41:03 +0100

    thread-as->

--- my-defuns/names.el
+++ my-defuns/names.el
@@ -24,16 +24,13 @@
                    (buffer-string))
                  "fooBarBaz\n\nfooBarBaz\nFooBarBaz\nfoobarbaz\nFOOBARBAZ\nfoo_bar_baz\nFOO_BAR_BAZ\nfoo-bar-baz")))
 
+(defun s-snake-upcase (s) (s-join "_" (mapcar 'upcase (s-split-words s))))
+(defun s-lowercase (s) (s-join "" (mapcar 'downcase (s-split-words s))))
+(defun s-upcase (s) (s-join "" (mapcar 'upcase (s-split-words s))))
+
 (defun names/create (names)
   "Create NAMES in different cases."
-  (cl-labels ((s-snake-upcase (s)
-                              (s-join "_" (mapcar 'upcase (s-split-words s))))
-              (s-lowercase (s)
-                           (s-join "" (mapcar 'downcase (s-split-words s))))
-              (s-upcase (s)
-                        (s-join "" (mapcar 'upcase (s-split-words s))))
-              (create-row (f)
-                          (s-join " " (mapcar f names))))
+  (cl-labels ((create-row (f) (s-join " " (mapcar f names))))
     (s-join "\n" (list (create-row 's-lower-camel-case)
                        (create-row 's-upper-camel-case)
                        (create-row 's-lowercase)
--- my-defuns/threading.el
+++ my-defuns/threading.el
@@ -4,11 +4,7 @@
 
 ;;; Code:
 
-(defmacro ->> (&rest body)
-  "The thread-first macro.  Threads the BODY."
-  (let ((result (pop body)))
-    (dolist (form body result)
-      (setq result (append form (list result))))))
+(require 'dash)
 
 (defmacro -> (&rest body)
   "The thread-last macro.  Threads the BODY."
@@ -16,5 +12,57 @@
     (dolist (form body result)
       (setq result (append (list (car form) result)
                            (cdr form))))))
+
+(ert-deftest test-threading-first ()
+  (should (equal
+           (-> ""
+               (concat "a")
+               (concat "b")
+               (concat "c"))
+           "abc")))
+
+(defmacro ->> (&rest body)
+  "The thread-first macro.  Threads the BODY."
+  (let ((result (pop body)))
+    (dolist (form body result)
+      (setq result (append form (list result))))))
+
+(ert-deftest test->> ()
+  (should (equal
+           (->> ""
+                (concat "a")
+                (concat "b")
+                (concat "c"))
+           "cba")))
+
+(defmacro as-> (&rest body)
+  "The thread-as macro.  Threads the BODY."
+  (let ((result (pop body))
+        (name (pop body)))
+    (dolist (form body result)
+      (setq result (append (-replace name result form))))))
+
+(ert-deftest test-first-as-> ()
+  (should (equal
+           (as-> "" v
+                 (concat v "a")
+                 (concat v "b")
+                 (concat v "c"))
+           "abc")))
+
+(ert-deftest test-last-as-> ()
+  (should (equal
+           (as-> "" v
+                 (concat "a" v)
+                 (concat "b" v)
+                 (concat "c" v))
+           "cba")))
+
+(ert-deftest test-mixed-as-> ()
+  (should (equal
+           (as-> "" v
+                 (concat "a" v)
+                 (concat v "1" v "2" v "3" v))
+           "a1a2a3a")))
 
 ;;; threading.el ends here
